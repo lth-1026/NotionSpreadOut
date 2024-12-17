8c4aba743e03bda26ee4947ac13699b4f0abeec0
lth-1026 <th20001026@gmail.com>
2024-12-18 00:38:57 +0900

    fetchNotionConetent 리팩토링
    MutationObserver를 이용해 동적으로 변하는 DOM 값을 감지할 수 있다.

--- scripts/content.js
+++ scripts/content.js
@@ -1,15 +1,58 @@
 // 페이지에서 텍스트 추출
 function getPageContent() {
     const pages = document.querySelectorAll('.notion-collection-item[data-block-id]'); 
-    for (let page of pages) {
+    pages.forEach((page) => {
         const id = page.getAttribute('data-block-id');
-        const pageContent = fetchNotionContent(id.replaceAll("-", ""));
-        console.log(pageContent);
-    }
-    return pageContent || 'Not found Data';
+        fetchNotionContent(id)
+            .then(content => console.log(content))
+            .catch(error => console.error(error));
+    })
+    // for (let page of pages) {
+    //     const id = page.getAttribute('data-block-id');
+    //     const pageContent = fetchNotionContent(id);
+    //     console.log(pageContent);
+    // }
+    // return pageContent || 'Not found Data';
 }
 
 async function fetchNotionContent(id) {
+    return new Promise((resolve, reject) => {
+        const currentDomain = window.location.origin;
+        const targetUrl = `${currentDomain}/${id.replaceAll('-', '')}`;
+
+        const iframe = document.createElement('iframe');
+        iframe.src = targetUrl;
+        iframe.style.display = 'none';
+
+        document.body.append(iframe);
+
+        iframe.onload = () => {
+            try {
+                const iframeDoc = iframe.contentWindow.document;
+
+                const observer = new MutationObserver(() => {
+                    const notionContent = iframeDoc.querySelector('.notion-page-content');
+                    if(notionContent) {
+                        console.log("notion content: ", notionContent.innerHTML);
+                        observer.disconnect();
+                        iframe.remove();
+                        resolve(notionContent.innerHTML);
+                    }
+                });
+
+                // iframe 내부 body 감지 시작
+                observer.observe(iframeDoc.body, {childList: true, subtree: true});
+            } catch (error) {
+                reject("Access denied or CORS issue");
+                iframe.remove();
+            }
+        };
+
+        iframe.onerror = () => {
+            reject("Failed to load iframe content.");
+            iframe.remove();
+        };
+    });
     // 현재 URL 가져오기
     const currentDomain = window.location.origin;
     const targetUrl = `${currentDomain}/${id}`;
